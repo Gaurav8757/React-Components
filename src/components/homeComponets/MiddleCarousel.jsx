859e751c564fca844ccd61ea02392ee5e8349565
Gaurav@123 <mr.gauravkumar8757@gmail.com>
2023-12-07 19:01:22 +0530

    adding more comp

--- src/components/homeComponets/MiddleCarousel.jsx
+++ src/components/homeComponets/MiddleCarousel.jsx
@@ -11,7 +11,7 @@ import 'swiper/css/pagination';
 const MiddleCarousel = ({ buy }) => {
   return (
     <section className="container-fluid bg-gray-500">
-      <div className='pt-10 ml-2 mr-2 bg-slate-100'>
+      <div className='pt-10 ml-2 mr-2  items-center bg-slate-100'>
         <Swiper
           // install Swiper modules
           modules={[Navigation, Pagination, Scrollbar, Autoplay]}
@@ -20,19 +20,19 @@ const MiddleCarousel = ({ buy }) => {
           navigation
           pagination={{ clickable: true }}
           autoplay={{
-            delay: 1000, // Set the delay in milliseconds between slides
+            delay: 5000, // Set the delay in milliseconds between slides
             disableOnInteraction: false, // Continue autoplay even when user interacts with the slider
           }}
           // scrollbar={{ draggable: true }}
           onSwiper={() => console.log("")}
-          onSlideChange={() => console.log('')}
-          
+          onSlideChange={() => true}
+          className='flex justify-center m-auto w-3/4  bg-slate-100 p-0 '
         >
           {
             buy.map((obj, idx) => (
               
-              <SwiperSlide>
-              <img src={obj.images} key={idx}/></SwiperSlide>
+              <SwiperSlide className='border bg-slate-400 w-30 rounded-2xl ' >
+              <img src={obj.images} key={idx}  className='w-52'/></SwiperSlide>
             ))
           }
 
