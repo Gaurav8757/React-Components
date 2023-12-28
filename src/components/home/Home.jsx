8c5941c50e0a9ad49fb68025f559cd3c111af633
Gaurav@123 <mr.gauravkumar8757@gmail.com>
2023-12-28 15:51:11 +0530

    arrange images

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -208,21 +208,25 @@ const Health = [{
 
 
 const general = [{
-    img: "/7.png"
+    img: "/1.png"
 },{
     img: "/2.png"
 },{
     img: "/3.png"
-},{
-    img: "/4.png"
+},
+{
+    img: "/4.jpg"
 },
 {
     img: "/5.png"
-},{
+},
+{
     img: "/6.png"
 },{
-    img: "/1.png"
-}
+    img: "/7.png"
+},{
+    img: "/8.png"
+},
 
 
 ]
