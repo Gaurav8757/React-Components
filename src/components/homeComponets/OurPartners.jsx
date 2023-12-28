b2ad9d693bc8ede8b8f479bc9d7a106c7ad2c5d8
Gaurav@123 <mr.gauravkumar8757@gmail.com>
2023-12-28 12:47:56 +0530

    update ourpartners two carousel

--- src/components/homeComponets/OurPartners.jsx
+++ src/components/homeComponets/OurPartners.jsx
@@ -8,17 +8,19 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 const OurPartners = ({Ourpartners}) => {
   return (
-    <section className="container-fluid  bg-gradient-to-r from-teal-500 to-indigo-400">
-        <div className="text-start text-black bg-gradient-to-r from-teal-500 to-indigo-400">
-        <div className="text-3xl mx-14 py-5 font-medium">Our Partners
-          <svg width="70" height="70" xmlns="http://www.w3.org/2000/svg" className="-mt-10 -ml-2">
+    <>
+    <main className='flex'>
+    <section className="container w-1/2 bg-gradient-to-r from-teal-500 to-teal-500">
+        <div className="text-start text-black bg-gradient-to-r from-teal-500 ">
+        <div className="text-3xl mx-14 py-5 font-medium">Health
+          <svg width="70" height="70" xmlns="http://www.w3.org/2000/svg" className="-mt-12 -ml-2">
             <line x1="10" y1="50" x2="90" y2="50" stroke="red" strokeWidth="4" />
           </svg>
         </div>
       <Swiper
         modules={[Navigation, Pagination, Scrollbar, Autoplay]}
-        spaceBetween={5}
-        slidesPerView={4} // Adjust the number of slides per view based on screen size
+        spaceBetween={0}
+        slidesPerView={2} // Adjust the number of slides per view based on screen size
         // navigation
         pagination={{ clickable: true }}
         autoplay={{
@@ -37,6 +39,38 @@ const OurPartners = ({Ourpartners}) => {
                     ))}
       </Swiper></div>
      </section>
+
+<section className="container w-1/2 bg-gradient-to-r from-teal-500 to-indigo-400">
+<div className="text-start text-black bg-gradient-to-r from-teal-500 to-indigo-400">
+<div className="text-3xl mx-14 py-5 font-medium">General Insurance
+  <svg width="70" height="70" xmlns="http://www.w3.org/2000/svg" className="-mt-12 -ml-2">
+    <line x1="10" y1="50" x2="90" y2="50" stroke="red" strokeWidth="4" />
+  </svg>
+</div>
+<Swiper
+modules={[Navigation, Pagination, Scrollbar, Autoplay]}
+spaceBetween={0}
+slidesPerView={2} // Adjust the number of slides per view based on screen size
+// navigation
+pagination={{ clickable: true }}
+autoplay={{
+  delay: 4000, // Set the delay in milliseconds between slides
+  disableOnInteraction: false, // Continue autoplay even when the user interacts with the slider
+}}
+className='container-fluid'>
+
+  {/* <div className="w-full max-w-md  border border-gray-200 rounded-lg shadow  dark:border-red-800"> */}
+  {Ourpartners.map((obj, idx) => (
+                <SwiperSlide className='rounded-2xl xl:pl-20 xl:pr-20 pb-10' key={idx}>
+                    <NavLink to="#">
+                        <img src={obj.img} className='w-full' alt={`slide-${idx}`} />
+                    </NavLink>
+                </SwiperSlide>
+            ))}
+</Swiper></div>
+</section>
+</main>
+</>
   );
 };
 export default OurPartners;
