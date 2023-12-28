139ce18a97dcb53cc50cad955fde7141ae361e09
Gaurav@123 <mr.gauravkumar8757@gmail.com>
2023-12-28 12:09:30 +0530

    update frontend

--- src/admin/admincomponents/ViewBranch.jsx
+++ src/admin/admincomponents/ViewBranch.jsx
@@ -62,7 +62,7 @@ export default function ViewBranch() {
         try {
           await axios.delete(`https://eleedomimf.onrender.com/dashboard/api/${_id}`);
           setAPIData((prevData) => prevData.filter((data) => data._id !== _id));
-          toast.warn(`${APIData.map((data) => data.branchname)} Branch Deleted...!`, { theme: "dark", position: "top-right" });
+          toast.warn(`${(prevData) => prevData.filter((data) => data.branchname)} Branch Deleted...!`, { theme: "dark", position: "top-right" });
         } catch (error) {
           console.error('Error deleting branch:', error);
         }
