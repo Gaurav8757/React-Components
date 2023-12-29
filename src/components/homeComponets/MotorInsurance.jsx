2f212b79f9d1478cd3f3115da3989d91ddcdbae6
Gaurav@123 <mr.gauravkumar8757@gmail.com>
2023-12-29 13:43:57 +0530

    update healths

--- src/components/homeComponets/MotorInsurance.jsx
+++ src/components/homeComponets/MotorInsurance.jsx
@@ -21,7 +21,7 @@ function MotorInsurance() {
 
   return (
     <section className="container-fluid bg-gradient-to-r from-indigo-400 to-cyan-400">
-      <div className="grid grid-cols-2 xs:grid-cols-3 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-2 justify-items-center pt-10 ml-2 mr-2 bg-gradient-to-r from-indigo-400 to-cyan-400">
+      <div className="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-6 gap-2 justify-items-center pt-10 ml-2 mr-2 bg-gradient-to-r from-indigo-400 to-cyan-400">
         {sublinks.map((data, idx) => (
           // adding link to click
           <NavLink to="#" className="grid w-52 h-52 mb-2 mr-8 ml-8 hover:-translate-y-1 hover:-translate-x-0" key={idx}>
