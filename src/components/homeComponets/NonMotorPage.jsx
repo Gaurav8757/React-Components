467161a5fdad0f18267653c00e6710c295d21a3e
Gaurav@123 <mr.gauravkumar8757@gmail.com>
2023-12-30 16:30:31 +0530

    update

--- src/components/homeComponets/NonMotorPage.jsx
+++ src/components/homeComponets/NonMotorPage.jsx
@@ -18,7 +18,7 @@ function NonMotorPage() {
             });
         // }
       }, []);
-
+console.log(APIData);
   return (
     <section className="container-fluid relative  h-screen p-0  bg-gradient-to-r from-indigo-400 to-cyan-400">
             <div className="container-fluid flex justify-center p-2  border-gray-200 border-dashed rounded-lg dark:border-gray-700  bg-gradient-to-r from-indigo-400 to-cyan-400">
