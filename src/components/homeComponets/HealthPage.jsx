f6a82f9b02d824f3f25872d9a80da4ec913c7ca3
Gaurav@123 <mr.gauravkumar8757@gmail.com>
2024-01-03 17:41:34 +0530

    created health modals with full function

--- src/components/homeComponets/HealthPage.jsx
+++ src/components/homeComponets/HealthPage.jsx
@@ -6,6 +6,7 @@ import axios from "axios";
 
 function HealthPage() {
     const [APIData, setAPIData] = useState([]);
+    // const [modalVisibility, setModalVisibility] = useState({});
     useEffect(() => {
         axios
             .get(`https://eleedomimf.onrender.com/api/company/health-list`)
@@ -20,63 +21,70 @@ function HealthPage() {
         // }
     }, []);
 
+
     return (
-        <section className="container-fluid relative  h-screen p-0  bg-gradient-to-r from-indigo-400 to-cyan-400">
-            <div className="container-fluid flex justify-center p-2  border-gray-200 border-dashed rounded-lg dark:border-gray-700  bg-gradient-to-r from-indigo-400 to-cyan-400">
+        <>
+            <section className="container-fluid relative  h-screen p-0  bg-gradient-to-r from-indigo-400 to-cyan-400">
+                <div className="container-fluid flex justify-center p-2  border-gray-200 border-dashed rounded-lg dark:border-gray-700  bg-gradient-to-r from-indigo-400 to-cyan-400">
 
-                {/* <div className="sm:-mx-6 lg:-mx-8"> */}
-                <div className="inline-block min-w-full w-full py-0 sm:px-6 lg:px-8">
-                    <div className="overflow-x-auto w-xl  text-white">
-                        <h1 className="flex justify-center text-4xl w-full mb-8">Health Insurance</h1><hr></hr>
-                    </div>
-                    <div className="inline-block min-w-full w-full py-0 sm:px-6 lg:px-8 overflow-x-auto">
-                        <table className="min-w-full text-center text-sm font-light ">
-                            <thead className="border-b font-medium dark:border-neutral-500">
-                                <tr className="text-white">
-                                    <th scope="col" className="px-5 py-4">
-                                        Company Name
-                                    </th>
+                    {/* <div className="sm:-mx-6 lg:-mx-8"> */}
+                    <div className="inline-block min-w-full w-full py-0 sm:px-6 lg:px-8">
+                        <div className="overflow-x-auto w-xl  text-white">
+                            <h1 className="flex justify-center text-4xl w-full mb-8">Health Insurance</h1><hr></hr>
+                        </div>
+                        <div className="inline-block min-w-full w-full py-0 sm:px-6 lg:px-8 overflow-x-auto">
+                            <table className="min-w-full text-center text-sm font-light ">
+                                <thead className="border-b font-medium dark:border-neutral-500">
+                                    <tr className="text-white">
+                                        <th scope="col" className="px-5 py-4">
+                                            Company Name
+                                        </th>
 
-                                    <th scope="col" className="px-5 py-4">
-                                        Files
-                                    </th>
-                                    <th scope="col" className="px-5 py-4">
+                                        <th scope="col" className="px-5 py-4">
+                                            Files
+                                        </th>
+                                        <th scope="col" className="px-5 py-4">
 
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {APIData.map((data) => {
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {APIData.map((data) => {
 
-                                    return (
-                                        <tr
-                                            className="border-b dark:border-neutral-200 text-sm font-medium"
-                                            key={data._id}>
-                                            <td className="whitespace-nowrap px-4 py-4">
-                                                {data.comp_cname}
-                                            </td>
+                                        return (
+                                            <tr
+                                                className="border-b dark:border-neutral-200 text-sm font-medium"
+                                                key={data._id}>
+                                                <td className="whitespace-nowrap px-4 py-4">
+                                                    {data.comp_cname}
+                                                </td>
 
-                                            <td className="whitespace-nowrap px4 py-4">
-                                                <NavLink to={`https://eleedomimf.com${data.comp_cfiles}`}>
-                                                    <img src={data.comp_cfiles} alt="files" />
-                                                </NavLink>
+                                                <td className="whitespace-nowrap px4 py-4">
+                                                    <NavLink to={`https://eleedomimf.com${data.comp_cfiles}`}>
+                                                        <img src={data.comp_cfiles} alt="files" />
+                                                    </NavLink>
 
-                                            </td>
-                                            <td className="whitespace-nowrap px-4 py-4">
-                                            <HealthForm />
-                                            </td>
+                                                </td>
+                                                <td className="whitespace-nowrap px-4 py-4">
 
-                                        </tr>
-                                    );
-                                })}
-                            </tbody>
-                        </table>
-                        {/* <div><HealthForm /></div> */}
+                                                    <HealthForm className="whitespace-nowrap px-4 py-4" />
+                                                </td>
+
+                                            </tr>
+
+                                        );
+                                    })}
+
+                                </tbody>
+
+                            </table>
+
+                        </div>
                     </div>
                 </div>
-            </div>
-            {/* </div> */}
-        </section>
+                {/* </div> */}
+            </section>
+        </>
     );
 
 }
--- src/components/homeComponets/ViewForm/HealthForm.jsx
+++ src/components/homeComponets/ViewForm/HealthForm.jsx
@@ -1,59 +1,78 @@
-import {NavLink} from "react-router-dom";
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import { IoMdClose } from "react-icons/io";
+import { useState } from "react";
 const HealthForm = () => {
-    return (
-        <>
-            <button data-modal-target="authentication-modal" data-modal-toggle="authentication-modal"
-                                                    className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-1 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2 text-center me-2 mb-2">
-                                                    Fill Form
-                                                </button>
+    const [showModal, setShowModal] = useState(false);
+  return (
+    <>
+      <button
+        className="bg-blue-200 text-black active:bg-blue-500 
+      font-bold px-4 py-2 rounded shadow  outline-none focus:outline-none mr-1 mb-1 transition-transform transform hover:translate-y-[-2px] hover:shadow-2xl"
+        type="button"
+        onClick={() => setShowModal(true)}
+      >
+        Fill Details
+      </button>
 
-            {/* // <!-- Main modal --> */}
-            <div id="authentication-modal" tabIndex="-1" aria-hidden="true" className="hidden overflow-y-auto overflow-x-hidden relative top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-                <div className="relative p-4 w-full max-w-md max-h-full">
-                    {/* <!-- Modal content --> */}
-                    <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                        {/* <!-- Modal header --> */}
-                        <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-                            <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                                Fill the Form
-                            </h3>
-                            <button type="button" className="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="authentication-modal">
-                                <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                                </svg>
-                                <span className="sr-only">Close modal</span>
-                            </button>
-                        </div>
-                        {/* <!-- Modal body --> */}
-                        <div className="p-4 md:p-5">
-                            <form className="space-y-4" action="#">
-                                <div>
-                                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                                    <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required />
-                                </div>
-                                <div>
-                                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-                                    <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
-                                </div>
-                                <div className="flex justify-between">
-                                    <div className="flex items-start">
-                                        <div className="flex items-center h-5">
-                                            <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
-                                        </div>
-                                        <label htmlFor="remember" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
-                                    </div>
-                                    <NavLink to="#" className="text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</NavLink>
-                                </div>
-                                <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
-                                <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-                                    Not registered? <NavLink to="#" className="text-blue-700 hover:underline dark:text-blue-500">Create account</NavLink>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
+      {showModal ? (
+        <>
+          <div className="flex justify-center  backdrop-blur-sm items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-1/3 my-6 mx-auto w-3xl ">
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-gradient-to-r from-teal-700 to-cyan-800 outline-none focus:outline-none">
+                <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t ">
+                  <h3 className="text-3xl font=semibold text-slate-200">Fill the Form</h3>
+                  <button
+                    className="bg-transparent border-0 text-black float-right"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="text-red-500 opacity-7 h-6 w-6 text-xl block  py-0 rounded-full transition duration-0 hover:duration-150">
+                      <IoMdClose  size={30}/>
+                    </span>
+                  </button>
                 </div>
+                <div className="relative p-6 flex-auto text-start bg-gradient-to-r from-teal-700 to-cyan-800">
+                  <form className="bg-gray-200 shadow-md rounded px-8 pt-6 pb-8 w-full bg-gradient-to-t from-green-600  to-teal-400">
+                    <label className="block text-black text-base font-bold mb-1">
+                      First Name
+                    </label>
+                    <input className="shadow appearance-none border rounded w-full py-2  px-1 mb-4 text-black" />
+                    <label className="block text-black text-base  font-bold mb-1">
+                      Last Name
+                    </label>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-1 mb-4 text-black" />
+                    <label className="block text-black text-base  font-bold mb-1">
+                      Address
+                    </label>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-1  mb-4 text-black" />
+                    <label className="block text-black text-base  font-bold mb-1">
+                      City
+                    </label>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-1 mb-4 text-black" />
+                  </form>
+                </div>
+                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                  {/* <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button> */}
+                  <button
+                    className="text-white bg-green-500  active:bg-green-700 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Submit
+                  </button>
+                </div>
+              </div>
             </div>
+          </div>
         </>
-    );
+      ) : null}
+    </>
+  );
 };
 export default HealthForm;
