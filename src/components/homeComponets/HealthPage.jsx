be89248fa140c04a6f3de180dc3337a185107740
Gaurav@123 <mr.gauravkumar8757@gmail.com>
2024-01-05 18:23:56 +0530

    user filled data based on selected company name both backend frontend of health insurance

--- src/components/homeComponets/HealthPage.jsx
+++ src/components/homeComponets/HealthPage.jsx
@@ -1,91 +1,93 @@
-import Form from "./ViewForm/Form.jsx";
+// HealthPage.jsx
 import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { toast } from "react-toastify";
 import axios from "axios";
+import Form from "./ViewForm/Form.jsx"
 
 function HealthPage() {
-    const [APIData, setAPIData] = useState([]);
-    // const [modalVisibility, setModalVisibility] = useState({});
-    useEffect(() => {
-        axios
-            .get(`https://eleedomimf.onrender.com/api/company/health-list`)
-            .then((response) => {
-                setAPIData(response.data);
+  const [APIData, setAPIData] = useState([]);
+  const [selectedCompanyName, setSelectedCompanyName] = useState("");
 
-            })
-            .catch((error) => {
-                toast.error(`No Data Found!`, { theme: "dark" })
-                console.error(error);
-            });
-        // }
-    }, []);
+  useEffect(() => {
+    axios
+      .get(`https://eleedomimf.onrender.com/api/company/health-list`)
+      .then((response) => {
+        setAPIData(response.data);
+      })
+      .catch((error) => {
+        toast.error(`No Data Found!`, { theme: "dark" });
+        console.error(error);
+      });
+  }, []);
 
+  const handleCompanySelection = (companyName) => {
+    setSelectedCompanyName(companyName);
+  };
 
-    return (
-        <>
-            <section className="container-fluid relative  h-screen p-0  bg-gradient-to-r from-indigo-400 to-cyan-400">
-                <div className="container-fluid flex justify-center p-2  border-gray-200 border-dashed rounded-lg dark:border-gray-700  bg-gradient-to-r from-indigo-400 to-cyan-400">
-
-                    {/* <div className="sm:-mx-6 lg:-mx-8"> */}
-                    <div className="inline-block min-w-full w-full py-0 sm:px-6 lg:px-8">
-                        <div className="overflow-x-auto w-xl  text-white">
-                            <h1 className="flex justify-center text-4xl w-full mb-8">Health Insurance</h1><hr></hr>
-                        </div>
-                        <div className="inline-block min-w-full w-full py-0 sm:px-6 lg:px-8 overflow-x-auto">
-                            <table className="min-w-full text-center text-sm font-light ">
-                                <thead className="border-b font-medium dark:border-neutral-500">
-                                    <tr className="text-white">
-                                        <th scope="col" className="px-5 py-4">
-                                            Company Name
-                                        </th>
-
-                                        <th scope="col" className="px-5 py-4">
-                                            Files
-                                        </th>
-                                        <th scope="col" className="px-5 py-4">
-
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {APIData.map((data) => {
-                                        return (
-                                            <tr
-                                                className="border-b dark:border-neutral-200 text-sm font-medium"
-                                                key={data._id}>
-                                                <td className="whitespace-nowrap px-4 py-4">
-                                                    {data.comp_cname}
-                                                </td>
-
-                                                <td className="whitespace-nowrap px4 py-4">
-                                                    <NavLink to={`https://eleedomimf.com${data.comp_cfiles}`}>
-                                                        <img src={data.comp_cfiles} alt="files" />
-                                                    </NavLink>
-
-                                                </td>
-                                                <td className="whitespace-nowrap px-4 py-4">
-
-                                                    <Form className="whitespace-nowrap px-4 py-4" />
-                                                </td>
-
-                                            </tr>
-
-                                        );
-                                    })}
-
-                                </tbody>
-
-                            </table>
-
-                        </div>
-                    </div>
-                </div>
-                {/* </div> */}
-            </section>
-        </>
-    );
-
+  return (
+    <>
+      <section className="container-fluid relative h-screen p-0 bg-gradient-to-r from-indigo-400 to-cyan-400">
+        <div className="container-fluid flex justify-center p-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 bg-gradient-to-r from-indigo-400 to-cyan-400">
+          <div className="inline-block min-w-full w-full py-0 sm:px-6 lg:px-8">
+            <div className="overflow-x-auto w-xl text-white">
+              <h1 className="flex justify-center text-4xl w-full mb-8">Health Insurance</h1>
+              <hr />
+            </div>
+            <div className="inline-block min-w-full w-full py-0 sm:px-6 lg:px-8 overflow-x-auto">
+              <table className="min-w-full text-center text-sm font-light">
+                <thead className="border-b font-medium dark:border-neutral-500">
+                  <tr className="text-white">
+                    <th scope="col" className="px-5 py-4">
+                      Company Name
+                    </th>
+                    <th scope="col" className="px-5 py-4">
+                      Files
+                    </th>
+                    <th scope="col" className="px-5 py-4">
+                      Fill Details
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {APIData.map((data) => {
+                    return (
+                      <tr
+                        className="border-b dark:border-neutral-200 text-sm font-medium"
+                        key={data._id}
+                      >
+                        <td className="whitespace-nowrap px-4 py-4">{data.comp_cname}</td>
+                        <td className="whitespace-nowrap px4 py-4">
+                          <NavLink to={`https://eleedomimf.com${data.comp_cfiles}`}>
+                            <img src={data.comp_cfiles} alt="files" />
+                          </NavLink>
+                        </td>
+                        <td className="whitespace-nowrap px-4 py-4">
+                          <button
+                            onClick={() => handleCompanySelection(data.comp_cname)}
+                            className="text-white bg-blue-700 active:bg-green-700 font-bold px-4 py-2 rounded shadow outline-none focus:outline-none mr-1 mb-1 transition-transform transform hover:translate-y-[-3px] hover:shadow-2xl"
+                            type="button"
+                          >
+                            Fill Details
+                          </button>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </section>
+      {selectedCompanyName && (
+        <Form
+          companyName={selectedCompanyName}
+          setShowModal={() => setSelectedCompanyName("")}
+        />
+      )}
+    </>
+  );
 }
 
 export default HealthPage;
--- src/components/homeComponets/ViewForm/Form.jsx
+++ src/components/homeComponets/ViewForm/Form.jsx
@@ -1,158 +1,146 @@
+/* eslint-disable react/prop-types */
+// Form.jsx
+
 import { IoMdClose } from "react-icons/io";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
-const Form = () => {
-  const [APIData, setAPIData] = useState([]);
+
+const Form = ({ companyName, setShowModal }) => {
   const [name, setName] = useState("");
-  const [mobile, setMobile] = useState();
+  const [mobile, setMobile] = useState("");
   const [email, setEmail] = useState("");
   const [address, setAddress] = useState("");
-  const [showModal, setShowModal] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [cname, setCname] = useState("");
+ 
 
   useEffect(() => {
     axios
       .get(`https://eleedomimf.onrender.com/api/company/health-list`)
       .then((response) => {
-        setAPIData(response.data);
-        console.log(response.data)
-
+        console.log(response.data);
+        // setAPIData(response.data);
       })
       .catch((error) => {
-        toast.error(`No Data Found!`, { theme: "dark" })
         console.error(error);
       });
-    // }
-  }, []);
-
-
-
-
-
-
+  }, [companyName]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setLoading(true);
+   
+
     try {
       // Make sure to replace this URL with your actual API endpoint
-      const response = await axios.post("https://eleedomimf.onrender.com/dashboard/addbranch", {
-
-        branchemail: email,
-        branchmobile: mobile,
-        branchaddress: address,
+      const response = await axios.post("https://eleedomimf.onrender.com/users/userDetails", {
+        h_cname: companyName,
+        h_name: name,
+        h_email: email,
+        h_mobile: mobile,
+        h_address: address,
       });
+
       if (response.data) {
-        toast.success("Added Successfully !");
+        toast.success("Added Successfully!");
 
         // Reset the form and loading state on successful submission
         setName("");
         setAddress("");
         setEmail("");
         setMobile("");
-        setLoading(false);
-      }
-      else {
-        toast.error("Error Occurred. Try again...! ");
+       
+
+        // Close the modal
+        setShowModal(false);
+      } else {
+        toast.error("Error Occurred. Try again...!");
       }
     } catch (error) {
-      console.error("Error during  registration:", error.response);
-      // setError("Error during branch registration. Please try again.");
-      setLoading(false);
+      console.error("Error during registration:", error.response);
+     
+    }finally {
+      
+      // Close the modal
+      setShowModal(false);
     }
   };
+
   return (
     <>
-      <button
-        className="bg-blue-200 text-black active:bg-blue-500 
-      font-bold px-4 py-2 rounded shadow  outline-none focus:outline-none mr-1 mb-1 transition-transform transform hover:translate-y-[-3px] hover:shadow-2xl"
-        type="button"
-        onClick={() => setShowModal(true)}
-      >
-        Fill Details
-      </button>
-
-      {showModal ? (
-        <>
-          <div className="flex justify-center  backdrop-blur-sm items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-            <div className="relative w-1/3 my-6 mx-auto w-3xl ">
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-gradient-to-r from-teal-700 to-cyan-800 outline-none focus:outline-none">
-                <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t ">
-                  <h3 className="text-3xl font=semibold text-slate-200">Fill the Form</h3>
-                  <button
-                    className="bg-transparent border-0 text-black float-right"
-                    onClick={() => setShowModal(false)}
-                  >
-                    <span className="text-red-500 opacity-7 h-6 w-6 text-xl block  py-0 rounded-full transition duration-0 hover:duration-150">
-                      <IoMdClose size={30} />
-                    </span>
-                  </button>
+      <div className="flex justify-center backdrop-blur-sm items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+        <div className="relative w-1/3 my-6 mx-auto w-3xl">
+          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-gradient-to-r from-teal-700 to-cyan-800 outline-none focus:outline-none">
+            <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t">
+              <h3 className="text-3xl font-semibold text-slate-200">Fill the Form</h3>
+              <button
+                className="bg-transparent border-0 text-black float-right"
+                onClick={() => setShowModal(false)}
+              >
+                <span className="text-red-500 opacity-7 h-6 w-6 text-xl block py-0 rounded-full transition duration-0 hover:duration-150">
+                  <IoMdClose size={30} />
+                </span>
+              </button>
+            </div>
+            <div className="relative p-6 flex-auto text-start bg-gradient-to-r from-teal-700 to-cyan-800">
+              <form className="bg-gray-200 shadow-md rounded px-8 pt-6 pb-8 w-full bg-gradient-to-t from-green-600  to-teal-400">
+                <div className="flex justify-between">
+                  <label className="inline text-black text-base font-bold mb-1">Name:</label>
+                  <label className="inline mx-24 text-black text-base font-bold mb-1">
+                    Company Name:
+                  </label>
                 </div>
-                <div className="relative p-6 flex-auto text-start bg-gradient-to-r from-teal-700 to-cyan-800">
-                  <form  className="bg-gray-200 shadow-md rounded px-8 pt-6 pb-8 w-full bg-gradient-to-t from-green-600  to-teal-400">
-                    <div className="flex justify-between">
-                      <label className="inline text-black text-base font-bold mb-1">
-                        Name:
-                      </label>
-                      <label className="inline  mx-24 text-black text-base font-bold mb-1">
-                        Company Name:
-                      </label></div>
-                    <div className="flex ">
-                      {/* name */}
-                      <input type="text" value={name}
-                        onChange={(e) => setName(e.target.value)} className="shadow appearance-none  border rounded w-full py-2  px-1 mb-4 text-black" />
-                      {/* company name */}
-                      <select type="text" value={cname}
-                        onChange={(e) => setCname(e.target.value)} className="shadow appearance-none ml-4 border rounded w-full py-2  px-1 mb-4 text-black" >
-                        <option className="disabled">---- Select Company ----</option>
-                        {APIData.map((health) => (
-                          <option key={health._id} value={health.comp_cname} className="text-base" >
-                            {health.comp_cname}
-                          </option>
-                          
-                        ))}
-
-                      </select>
-                      
-                    </div>
-                    <label className="block text-black text-base  font-bold mb-1">
-                      Mobile:
-                    </label>
-                    <input type="number" value={mobile}
-                      onChange={(e) => setMobile(e.target.value)} className="shadow appearance-none border rounded w-full py-2 px-1 mb-4 text-black" />
-                    <label className="block text-black text-base  font-bold mb-1">
-                      Email:
-                    </label>
-                    <input type="email" value={email}
-                      onChange={(e) => setEmail(e.target.value)} className="shadow appearance-none border rounded w-full py-2 px-1 mb-4 text-black" />
-                    <label className="block text-black text-base  font-bold mb-1">
-                      Address<span className="font-thin text-white"> (Optional)</span>
-                    </label>
-                    <textarea value={address}
-                      onChange={(e) => setAddress(e.target.value)} className="shadow appearance-none border rounded w-full py-2 px-1  mb-4 text-black" />
-
-                  </form>
+                <div className="flex">
+                  <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className="shadow appearance-none border rounded w-full py-2 px-1 mb-4 text-black"
+                  />
+                  <input
+                    type="text"
+                    value={companyName}
+                    onChange={(e) => setName(e.target.value)}
+                    className="shadow appearance-none border rounded w-full py-2 px-1 mb-4 ml-4 text-black"
+                    readOnly
+                  />
                 </div>
-                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
-                  <button
-                    className="text-white bg-green-500  active:bg-green-700 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
-                    type="button"
-                    onClick={async () => {
-                      await handleSubmit();
-                      setShowModal(false);
-                  }}
-                  >
-                    Submit
-                  </button>
-                </div>
-              </div>
+                <label className="block text-black text-base font-bold mb-1">Mobile:</label>
+                <input
+                  type="number"
+                  value={mobile}
+                  onChange={(e) => setMobile(e.target.value)}
+                  className="shadow appearance-none border rounded w-full py-2 px-1 mb-4 text-black"
+                />
+                <label className="block text-black text-base font-bold mb-1">Email:</label>
+                <input
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="shadow appearance-none border rounded w-full py-2 px-1 mb-4 text-black"
+                />
+                <label className="block text-black text-base font-bold mb-1">
+                  Address<span className="font-thin text-white"> (Optional)</span>
+                </label>
+                <textarea
+                  value={address}
+                  onChange={(e) => setAddress(e.target.value)}
+                  className="shadow appearance-none border rounded w-full py-2 px-1 mb-4 text-black"
+                />
+              </form>
+            </div>
+            <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+              <button
+                className="text-white bg-green-500  active:bg-green-700 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
+                type="button"
+                onClick={handleSubmit}
+              >
+                Submit
+              </button>
             </div>
           </div>
-        </>
-      ) : loading}
+        </div>
+      </div>
     </>
   );
 };
+
 export default Form;
