6d2a84b65af298d497a0d07df2c5629985aa25b0
Gaurav@123 <mr.gauravkumar8757@gmail.com>
2024-01-16 11:27:39 +0530

    update

--- src/admin/admincomponents/uploadCarousel/ViewCarousel.jsx
+++ src/admin/admincomponents/uploadCarousel/ViewCarousel.jsx
@@ -111,10 +111,10 @@ function ViewCarousel() {
                                                 {data.usercarousel_link}
                                             </td>
 
-                                            <td className="whitespace-nowrap px4 py-4">
-                                                <img src={`https://eleedomimf.onrender.com${data.usercarousel_upload}`} alt="file" />
+                                            <td className="whitespace-nowrap px-4 py-4 justify-center flex">
+                                                <img src={`https://eleedomimf.onrender.com${data.usercarousel_upload}`} className="w-1/2 " alt="file" />
                                             </td>
-                                            
+
                                             <td className="whitespace-nowrap px-4 py-4">
                                                 <Link to="#">
                                                     <button type="button" onClick={() => setData(data)} className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-1 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2 text-center me-2 mb-2 ">
