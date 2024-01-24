5eae3b054c0819619214148f710e816a1092cb20
Gaurav@123 <mr.gauravkumar8757@gmail.com>
2024-01-24 17:10:38 +0530

    update

--- src/admin/admincomponents/Branch/ViewBranch.jsx
+++ src/admin/admincomponents/Branch/ViewBranch.jsx
@@ -135,7 +135,7 @@ export default function ViewBranch() {
                                             <td className="whitespace-nowrap px4 py-4">
                                                 {data.branchemail}
                                             </td>
-                                            <td className="flex-1 whitespace-wrap px-4 py-4">
+                                            <td className="whitespace-wrap px-4 py-4">
                                                 {data.password}
                                             </td>
                                             <td className="whitespace-nowrap px-4 py-4">
@@ -159,12 +159,8 @@ export default function ViewBranch() {
                                             <td className="whitespace-nowrap px-4 py-4">
                                                 {data.branchpincode} 
                                             </td>
-                                            <td className="whitespace-nowrap px-4 py-4">
-                                               
-                                                   
+                                            <td className="whitespace-nowrap px-4 py-4"> 
                                                         <UpdateBranch branch = {data} onUpdate={onUpdateBranch}/> 
-                                                   
-                                                
                                             </td>
                                             <td className="whitespace-nowrap px-4 py-4">
                                                 <button type="button" onClick={() => onDeleteBranch(data._id)} className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-1 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2 text-center me-2 mb-2">Delete</button>
