f688f96aa3b6fa47b03d2fa6d7ea4f92b4c66a70
Gaurav@123 <mr.gauravkumar8757@gmail.com>
2024-01-30 19:08:32 +0530

    update

--- src/Employee/EmpAttendance.jsx
+++ src/Employee/EmpAttendance.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import Calendar from 'react-calendar';
-import '/policy-bazaar/src/employee/EmpAttendance.css';
+// import '/policy-bazaar/src/employee/EmpAttendance.css';
 import axios from 'axios';
 
 function EmpAttendance() {
