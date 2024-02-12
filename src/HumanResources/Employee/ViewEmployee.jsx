9e89f425c251ed86e7797cb2a8d890f14580e81f
Gaurav@123 <mr.gauravkumar8757@gmail.com>
2024-02-12 14:09:22 +0530

    update delete hr

--- src/HumanResources/Employee/ViewEmployee.jsx
+++ src/HumanResources/Employee/ViewEmployee.jsx
@@ -135,9 +135,9 @@ export default function ViewEmployee() {
                                     <th scope="col" className="px-5 py-4">
                                         Attendance
                                     </th>
-                                    <th scope="col" className="px-5 py-4">
+                                    {/* <th scope="col" className="px-5 py-4">
                                         Delete
-                                    </th>
+                                    </th> */}
                                 </tr>
                             </thead>
                             <tbody>
@@ -206,9 +206,9 @@ export default function ViewEmployee() {
                                                <EmpAttendanceModal  emp = {data.employeeDetails}  />
                                             </td>
 
-                                            <td className="whitespace-nowrap px-4 py-4">
-                                                <button type="button" onClick={() => onDeleteEmployee(data._id)} className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-1 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2 text-center me-2 mb-2">Delete</button>
-                                            </td>
+                                            {/* <td className="whitespace-nowrap px-4 py-4">
+                                                <button type="button" onClick={() => onDeleteEmployee(data.id)} className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-1 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2 text-center me-2 mb-2">Delete</button>
+                                            </td> */}
                                         </tr>
                                     );
                                 })}
