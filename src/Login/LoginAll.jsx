9b654882a1b810e8c63501b6d981372c1b684bcc
Gaurav@123 <mr.gauravkumar8757@gmail.com>
2024-02-17 16:11:22 +0530

    update ui of loginpage

--- src/Login/LoginAll.jsx
+++ src/Login/LoginAll.jsx
@@ -175,18 +175,18 @@ function LoginAll() {
     return (
         <>
             <section className="container-fluid h-screen relative" style={{ backgroundColor: "#f37e20" }}>
-                <div className="container-fluid pt-20 flex flex-col md:flex-row items-center pb-16 justify-between" style={{ backgroundColor: "#f37e20" }}>
-                    <div className="flex-shrink-4 px-6 md:h-full h-full py-20">
+                <div className="container-fluid pt-1 flex flex-col md:flex-row items-center pb-16 justify-between" style={{ backgroundColor: "#f37e20" }}>
+                    <div className="flex-shrink-4 md:h-full h-full w-full xs:w-full  sm:w-full md:w-full mx-auto lg:w-2/5 xl:py-20">
                         <img
                             src="/ins.png"
-                            className="h-1/4 w-full rounded-md mx-auto "
+                            className="h-full mt-20 w-full rounded-md mx-auto "
                             alt="Logo"
                         />
                         <div className="text-4xl font-bold mt-3 w-64 mx-auto  text-black-700 flex justify-center">Login</div>
                     </div>
 
-                    <div className="flex-shrink-1  md:h-1/4 h-full w-full xs:w-full  sm:w-full md:1/2 mx-auto lg:w-1/3 xl:w-1/4 xl:py-20">
-                        <div className="w-full max-w-xl p-6 space-y-1 sm:p-8  rounded-lg shadow bg-slate-100 ">
+                    <div className="flex-shrink-1 mt-10 md:h-1/4 h-full w-full xs:w-full  sm:w-full md:1/2 mx-auto lg:w-1/3 xl:w-1/4 xl:py-20">
+                        <div className="w-full max-w-xl p-6 space-y-1 sm:p-8 mx-auto  rounded-lg shadow bg-slate-100 ">
                             <img
                                 src="/logo.jpg"
                                 className="h-1/4 w-1/4  mx-auto "
